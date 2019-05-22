1a0d623fbc0a00e95498e1f9dcca486c4c011e57
PC-50 <vuvandat96@gmail.com>
2019-05-22 20:47:45 +0700

    111

---
+++ day1/index11.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h1>Các hàm xử lý chuỗi trong php</h1>
+    <pre>
+        - str_replace("chuỗi cần tìm và bị thay thế", "chuỗi thay thế", "chuỗi mẹ") thay thế chuỗi 1 bằng chuỗi 2 trong 1 chuỗi mẹ
+
+    </pre>
+
+    <?php
+    echo str_replace("ha noi", "da nang", "chao mung ban den tp ha noi");
+    ?>
+
+</body>
+</html>
